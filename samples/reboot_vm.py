7ecd1fc1726b1d4078059044dc1e867c532f9caa
Michael Rice <michael.rice@rackspace.com>
2014-05-28 01:32:11 -0500

    added requests to the requirements. the marvel example uses it. changes to reboot example.

--- samples/reboot_vm.py
+++ samples/reboot_vm.py
@@ -1,21 +1,24 @@
-# Written by Michael Rice
-# Github: https://github.com/michaelrice
-# Website: https://michaelrice.github.io/
-# Blog: http://www.errr-online.com/
-# This code has been released under the terms of the MIT licenses
-# http://opensource.org/licenses/MIT
+"""Written by Michael Rice
+Github: https://github.com/michaelrice
+Website: https://michaelrice.github.io/
+Blog: http://www.errr-online.com/
+This code has been released under the terms of the MIT licenses
+http://opensource.org/licenses/MIT
+
+Example script to reboot a VirtualMachine
+
+"""
 __author__ = 'errr'
 
 from tools import cli
-from tools import taskops
+from tools import tasks
 from pyVim import connect
 
 import atexit
 
 
 def setup_args():
-    """
-    Adds additional args to allow the vm name or uuid to
+    """Adds additional ARGS to allow the vm name or uuid to
     be set.
     """
     parser = cli.build_arg_parser()
@@ -23,41 +26,46 @@ def setup_args():
     parser.add_argument('-j', '--uuid',
                         help='UUID of the VirtualMachine you want to reboot.')
     parser.add_argument('-n', '--name',
-                        help='DNS Name of the VirtualMachine you want to reboot.')
+                        help='DNS Name of the VirtualMachine you want to '
+                             'reboot.')
     parser.add_argument('-i', '--ip',
-                        help='IP Address of the VirtualMachine you want to reboot')
+                        help='IP Address of the VirtualMachine you want to '
+                             'reboot')
 
     my_args = parser.parse_args()
 
     return cli.prompt_for_password(my_args)
 
 
-args = setup_args()
-si = None
+ARGS = setup_args()
+SI = None
 try:
-    si = connect.SmartConnect(host=args.host,
-                              user=args.user,
-                              pwd=args.password,
-                              port=int(args.port))
-    atexit.register(connect.Disconnect, si)
-except IOError, e:
+    SI = connect.SmartConnect(host=ARGS.host,
+                              user=ARGS.user,
+                              pwd=ARGS.password,
+                              port=ARGS.port)
+    atexit.register(connect.Disconnect, SI)
+except IOError, ex:
     pass
 
-if not si:
+if not SI:
     raise SystemExit("Unable to connect to host with supplied info.")
-vm = None
-if args.uuid:
-    vm = si.content.searchIndex.FindByUuid(None, args.uuid, True)
-elif args.name:
-    vm = si.content.searchIndex.FindByDnsName(None, args.name, True)
-elif args.ip:
-    vm = si.content.searchIndex.FindByIp(None, args.ip, True)
+VM = None
+if ARGS.uuid:
+    VM = SI.content.searchIndex.FindByUuid(None, ARGS.uuid,
+                                           True,
+                                           True)
+elif ARGS.name:
+    VM = SI.content.searchIndex.FindByDnsName(None, ARGS.name,
+                                              True)
+elif ARGS.ip:
+    VM = SI.content.searchIndex.FindByIp(None, ARGS.ip, True)
 
-if vm is None:
+if VM is None:
     raise SystemExit("Unable to locate VirtualMachine.")
 
-print "Found: {0}".format(vm.name)
-print "The current powerState is: {0}".format(vm.runtime.powerState)
-task = vm.ResetVM_Task()
-taskops.wait_for_tasks([task], si)
+print "Found: {0}".format(VM.name)
+print "The current powerState is: {0}".format(VM.runtime.powerState)
+TASK = VM.ResetVM_Task()
+tasks.wait_for_tasks(SI, [TASK])
 print "its done."
