aa8695b12f35aebfbdf194446603a790b5a2070f
bobfraser1 <bobfraser1@gmail.com>
2014-08-01 13:52:30 -0700

    Add navigation and simple templating.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+$(document).ready(function() {
+	$.get('/pyvmomi-community-samples/templates/navbar.html', function(data) {
+		$('#page_wrap').prepend(data);
+	});
+	$.get('/pyvmomi-community-samples/templates/footer.html', function(data) {
+		$('#page_wrap').append(data);
+	});
+});
