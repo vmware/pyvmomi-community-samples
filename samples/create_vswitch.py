c91f4f6a8f6e3beb35058887b57c2388cb0c692f
Reubenur Rahman <reuben.13@gmail.com>
2014-08-11 16:20:31 +0530

    Incorporated review comments given by @michaelrice

--- samples/create_vswitch.py
+++ samples/create_vswitch.py
@@ -5,7 +5,7 @@ Github: https://github.com/rreubenur/
 This code is released under the terms of the Apache 2
 http://www.apache.org/licenses/LICENSE-2.0.html
 
-Example script to change the network of the Virtual Machine NIC
+Example script to create a Vsphere Standard Switch
 
 """
 
@@ -13,54 +13,20 @@ import atexit
 import argparse
 import getpass
 
+from tools import cli
 from tools import tasks
 from pyVim import connect
 from pyVmomi import vim, vmodl
 
-def get_obj(content, vimtype, name):
-    """
-     Get the vsphere object associated with a given text name
-    """    
-    obj = None
-    container = content.viewManager.CreateContainerView(content.rootFolder, vimtype, True)
-    for c in container.view:
-        if c.name == name:
-            obj = c
-            break
-    return obj
-
 def get_args():
     """Get command line args from the user.
     """
-    parser = argparse.ArgumentParser(
-        description='Standard Arguments for talking to vCenter')
-    # because -h is reserved for 'help' we use -s for service
-    parser.add_argument('-s', '--host',
-                        required=True,
-                        action='store',
-                        help='vSphere service to connect to')
-
-    # because we want -p for password, we use -o for port
-    parser.add_argument('-o', '--port',
-                        type=int,
-                        default=443,
-                        action='store',
-                        help='Port to connect on')
-
-    parser.add_argument('-u', '--user',
-                        required=True,
-                        action='store',
-                        help='User name to use when connecting to host')
-
-    parser.add_argument('-p', '--password',
-                        required=False,
-                        action='store',
-                        help='Password to use when connecting to host')
+    parser = cli.build_arg_parser()
     
-    parser.add_argument('-d', '--host_name',
-                        required=False,
-                        action='store',
-                        help='Esxi host name')
+    parser.add_argument('-e', '--esx_host',
+                        required=True,
+                        help='Name/IP of the Physical Host you want to add the '
+                             'vSwitch.')
 
     parser.add_argument('-t', '--vswitch_name',
                         required=False,
@@ -112,7 +78,13 @@ def main():
         atexit.register(connect.Disconnect, service_instance)
         content = service_instance.RetrieveContent()
 
-        host = get_obj(content, [vim.HostSystem], args.host_name)
+        if not service_instance:
+            raise SystemExit("Unable to connect to host with supplied info.")
+
+        host = service_instance.content.searchIndex.FindByIp(None, args.esx_host, False)
+
+        if not host:
+            raise SystemExit("Unable to locate Physical Host.")
 
         host_network_system = host.configManager.networkSystem
 
