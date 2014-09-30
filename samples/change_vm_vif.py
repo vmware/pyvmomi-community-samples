f45fdcb2a0aa88546f597a46f104e246f879ee55
Reubenur Rahman <reuben.13@gmail.com>
2014-09-30 12:07:13 +0530

    Update and rename change_vm_vif to change_vm_vif.py

--- samples/change_vm_vif.py
+++ samples/change_vm_vif.py
@@ -10,71 +10,57 @@ Example script to change the network of the Virtual Machine NIC
 """
 
 import atexit
-import argparse
 import getpass
 
+from tools import cli
 from tools import tasks
 from pyVim import connect
 from pyVmomi import vim, vmodl
 
+
 def get_obj(content, vimtype, name):
     """
      Get the vsphere object associated with a given text name
-    """    
+    """
     obj = None
-    container = content.viewManager.CreateContainerView(content.rootFolder, vimtype, True)
-    for c in container.view:
-        if c.name == name:
-            obj = c
+    container = content.viewManager.CreateContainerView(content.rootFolder,
+                                                        vimtype, True)
+    for view in container.view:
+        if view.name == name:
+            obj = view
             break
     return obj
 
+
 def str2bool(v):
-  #susendberg's function
-  return v.lower() in ("yes", "true", "t", "1")
+    """
+    String to Bool function
+    """
+    return v.lower() in ("yes", "true", "t", "1")
+
 
 def get_args():
     """Get command line args from the user.
     """
-    parser = argparse.ArgumentParser(
-        description='Standard Arguments for talking to vCenter')
-    parser.register('type', 'bool', str2bool)
-    # because -h is reserved for 'help' we use -s for service
-    parser.add_argument('-s', '--host',
-                        required=True,
-                        action='store',
-                        help='vSphere service to connect to')
 
-    # because we want -p for password, we use -o for port
-    parser.add_argument('-o', '--port',
-                        type=int,
-                        default=443,
-                        action='store',
-                        help='Port to connect on')
-
-    parser.add_argument('-u', '--user',
-                        required=True,
-                        action='store',
-                        help='User name to use when connecting to host')
-
-    parser.add_argument('-p', '--password',
+    parser = cli.build_arg_parser()
+    parser.add_argument('-c', '--datacenter',
                         required=False,
                         action='store',
-                        help='Password to use when connecting to host')
-    
-    parser.add_argument('-v', '--vm_name',
+                        help='Datacenter Name')
+    parser.add_argument('-v', '--vm_uuid',
                         required=False,
                         action='store',
-                        help='Virtual machine name')
+                        help='Virtual machine uuid')
 
     parser.add_argument('-n', '--network_name',
                         required=False,
                         action='store',
-                        help='Network/Portgroup name')
+                        help='Name of the network/portgroup')
 
-    parser.add_argument('-d', '--is_distributed',
+    parser.add_argument('-d', '--is_VDS',
                         type=str2bool,
-                        help='Is Distributed type')     
+                        help='The provided network is in VSS or VDS')
 
     args = parser.parse_args()
 
@@ -100,31 +86,44 @@ def main():
 
         atexit.register(connect.Disconnect, service_instance)
         content = service_instance.RetrieveContent()
-
-        vm = get_obj(content, [vim.VirtualMachine], args.vm_name)
-        #This code is for changing only one Interface. For multiple Interface 
-        #Iterate through a loop of network names.
+        for child in content.rootFolder.childEntity:
+            if hasattr(child, 'hostFolder'):
+                datacenter = child
+                if datacenter.name == args.datacenter:
+                    break
+        vm = content.searchIndex.FindByUuid(datacenter, args.vm_uuid, True)
+        # This code is for changing only one Interface. For multiple Interface
+        # Iterate through a loop of network names.
         device_change = []
         for device in vm.config.hardware.device:
             if isinstance(device, vim.vm.device.VirtualEthernetCard):
                 nicspec = vim.vm.device.VirtualDeviceSpec()
-                nicspec.operation = vim.vm.device.VirtualDeviceSpec.Operation.edit
+                nicspec.operation = \
+                    vim.vm.device.VirtualDeviceSpec.Operation.edit
                 nicspec.device = device
                 nicspec.device.wakeOnLanEnabled = True
 
-                if not args.is_distributed:
-                    nicspec.device.backing = vim.vm.device.VirtualEthernetCard.NetworkBackingInfo()
-                    nicspec.device.backing.network = get_obj(content, [vim.Network], args.network_name)
+                if not args.is_VDS:
+                    nicspec.device.backing = \
+                        vim.vm.device.VirtualEthernetCard.NetworkBackingInfo()
+                    nicspec.device.backing.network = \
+                        get_obj(content, [vim.Network], args.network_name)
                     nicspec.device.backing.deviceName = args.network_name
                 else:
-                    network = get_obj(content, [vim.dvs.DistributedVirtualPortgroup], args.network_name)
+                    network = get_obj(content,
+                                      [vim.dvs.DistributedVirtualPortgroup],
+                                      args.network_name)
                     dvs_port_connection = vim.dvs.PortConnection()
                     dvs_port_connection.portgroupKey = network.key
-                    dvs_port_connection.switchUuid = network.config.distributedVirtualSwitch.uuid
-                    nicspec.device.backing = vim.vm.device.VirtualEthernetCard.DistributedVirtualPortBackingInfo()
+                    dvs_port_connection.switchUuid = \
+                        network.config.distributedVirtualSwitch.uuid
+                    nicspec.device.backing = \
+                        vim.vm.device.VirtualEthernetCard. \
+                        DistributedVirtualPortBackingInfo()
                     nicspec.device.backing.port = dvs_port_connection
-                
-                nicspec.device.connectable = vim.vm.device.VirtualDevice.ConnectInfo()
+
+                nicspec.device.connectable = \
+                    vim.vm.device.VirtualDevice.ConnectInfo()
                 nicspec.device.connectable.startConnected = True
                 nicspec.device.connectable.allowGuestControl = True
                 device_change.append(nicspec)
