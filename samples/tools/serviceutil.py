6ba5b208c8a5d3eb43b31e4e3818d54d068f63a7
Jonathan Dye <jonathan.dye@sungard.com>
2014-10-16 16:25:41 -0600

    fix waitforupdates sample issues for merging relocate serviceutil port to tools, update imports add option to disable ssl verification warnings since switch to requests add sample usage formatting changes for pep8

--- samples/tools/serviceutil.py
+++ samples/tools/serviceutil.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-#
 # VMware vSphere Python SDK
 # Copyright (c) 2008-2014 VMware, Inc. All Rights Reserved.
 #
@@ -25,18 +23,21 @@ from pyVmomi import vim, vmodl
 
 
 def buildFullTraversal():
-    '''
-    Builds a traversal spec that will recurse through all objects .. or at least
-    I think it does. additions welcome.
+    """
+    Builds a traversal spec that will recurse through all objects .. or at
+    least I think it does. additions welcome.
 
-    See com.vmware.apputils.vim25.ServiceUtil.buildFullTraversal in the java API.
-    Extended by Sebastian Tello's examples from pysphere to reach networks and datastores.
-    '''
+    See com.vmware.apputils.vim25.ServiceUtil.buildFullTraversal in the java
+    API. Extended by Sebastian Tello's examples from pysphere to reach networks
+    and datastores.
+    """
+
     TraversalSpec = vmodl.query.PropertyCollector.TraversalSpec
     SelectionSpec = vmodl.query.PropertyCollector.SelectionSpec
 
     # Recurse through all resourcepools
-    rpToRp = TraversalSpec(name='rpToRp', type=vim.ResourcePool, path="resourcePool", skip=False)
+    rpToRp = TraversalSpec(name='rpToRp', type=vim.ResourcePool,
+                           path="resourcePool", skip=False)
 
     rpToRp.selectSet.extend(
         (
@@ -45,10 +46,12 @@ def buildFullTraversal():
         )
     )
 
-    rpToVm = TraversalSpec(name='rpToVm', type=vim.ResourcePool, path="vm", skip=False)
+    rpToVm = TraversalSpec(name='rpToVm', type=vim.ResourcePool, path="vm",
+                           skip=False)
 
     # Traversal through resourcepool branch
-    crToRp = TraversalSpec(name='crToRp', type=vim.ComputeResource, path='resourcePool', skip=False)
+    crToRp = TraversalSpec(name='crToRp', type=vim.ComputeResource,
+                           path='resourcePool', skip=False)
     crToRp.selectSet.extend(
         (
             SelectionSpec(name='rpToRp'),
@@ -57,10 +60,12 @@ def buildFullTraversal():
     )
 
     # Traversal through host branch
-    crToH = TraversalSpec(name='crToH', type=vim.ComputeResource, path='host', skip=False)
+    crToH = TraversalSpec(name='crToH', type=vim.ComputeResource, path='host',
+                          skip=False)
 
     # Traversal through hostFolder branch
-    dcToHf = TraversalSpec(name='dcToHf', type=vim.Datacenter, path='hostFolder', skip=False)
+    dcToHf = TraversalSpec(name='dcToHf', type=vim.Datacenter,
+                           path='hostFolder', skip=False)
     dcToHf.selectSet.extend(
         (
             SelectionSpec(name='visitFolders'),
@@ -68,7 +73,8 @@ def buildFullTraversal():
     )
 
     # Traversal through vmFolder branch
-    dcToVmf = TraversalSpec(name='dcToVmf', type=vim.Datacenter, path='vmFolder', skip=False)
+    dcToVmf = TraversalSpec(name='dcToVmf', type=vim.Datacenter,
+                            path='vmFolder', skip=False)
     dcToVmf.selectSet.extend(
         (
             SelectionSpec(name='visitFolders'),
@@ -76,7 +82,8 @@ def buildFullTraversal():
     )
 
     # Traversal through network folder branch
-    dcToNet = TraversalSpec(name='dcToNet', type=vim.Datacenter, path='networkFolder', skip=False)
+    dcToNet = TraversalSpec(name='dcToNet', type=vim.Datacenter,
+                            path='networkFolder', skip=False)
     dcToNet.selectSet.extend(
         (
             SelectionSpec(name='visitFolders'),
@@ -84,7 +91,8 @@ def buildFullTraversal():
     )
 
     # Traversal through datastore branch
-    dcToDs = TraversalSpec(name='dcToDs', type=vim.Datacenter, path='datastore', skip=False)
+    dcToDs = TraversalSpec(name='dcToDs', type=vim.Datacenter,
+                           path='datastore', skip=False)
     dcToDs.selectSet.extend(
         (
             SelectionSpec(name='visitFolders'),
@@ -92,7 +100,8 @@ def buildFullTraversal():
     )
 
     # Recurse through all hosts
-    hToVm = TraversalSpec(name='hToVm', type=vim.HostSystem, path='vm', skip=False)
+    hToVm = TraversalSpec(name='hToVm', type=vim.HostSystem, path='vm',
+                          skip=False)
     hToVm.selectSet.extend(
         (
             SelectionSpec(name='visitFolders'),
@@ -100,8 +109,8 @@ def buildFullTraversal():
     )
 
     # Recurse through the folders
-    visitFolders = TraversalSpec(name='visitFolders', type=vim.Folder, path='childEntity',
-                                 skip=False)
+    visitFolders = TraversalSpec(name='visitFolders', type=vim.Folder,
+                                 path='childEntity', skip=False)
     visitFolders.selectSet.extend(
         (
             SelectionSpec(name='visitFolders'),
@@ -117,9 +126,10 @@ def buildFullTraversal():
     )
 
     fullTraversal = SelectionSpec.Array(
-        (visitFolders, dcToHf, dcToVmf, dcToNet, crToH, crToRp, dcToDs, rpToRp, hToVm, rpToVm,))
+        (visitFolders, dcToHf, dcToVmf, dcToNet, crToH, crToRp, dcToDs, rpToRp,
+         hToVm, rpToVm,))
 
     return fullTraversal
 
 
-# vim: set ts=2 sw=2 expandtab filetype=python:
+# vim: set ts=4 sw=4 expandtab filetype=python:
