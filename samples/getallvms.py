7f5bad34aecf93be3049d25623c69a94005a2118
Luis San Martin <lsanmartinr@gmail.com>
2015-05-12 17:10:23 -0300

    Additional info about vmware-tools, ipAdress
    Add vmware-tools info and ipAddress if none is found

--- samples/getallvms.py
+++ samples/getallvms.py
@@ -55,8 +55,15 @@ def print_vm_info(virtual_machine, depth=1):
     print "State      : ", summary.runtime.powerState
     if summary.guest is not None:
         ip_address = summary.guest.ipAddress
+        tools_version = summary.guest.toolsStatus
+        if tools_version is not None:
+            print "VMware-tools: ", tools_version
+        else:
+            print "Vmware-tools: None"
         if ip_address:
             print "IP         : ", ip_address
+        else:
+            print "IP         : None"
     if summary.runtime.question is not None:
         print "Question  : ", summary.runtime.question.text
     print ""
