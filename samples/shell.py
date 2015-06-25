6bb605d425f34a7e8f0ec964f579356fd47f3fb3
duff0097 <duff0097@umn.edu>
2015-06-25 10:08:42 -0500

    a python "shell" to aid in prototyping scripts
    The whole point is to aid prototyping scripts:
    1) Content to a vcenter right away
    2) have a easy way to re-authenticate
    3) have a easy way to pull objects
    
    Those were the most common things I ran into.  I'm sure more can be
    added, but that's it for the moment.

---
+++ samples/shell.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import code
+import atexit
+
+from pyVim import connect
+from pyVmomi import vmodl
+from pyVmomi import vim
+
+from tools import cli
+from tools import vm
+
+def get_view(content, vimtype):
+    """
+    """
+    objview = content.viewManager.CreateContainerView(content.rootFolder, vimtype, True)
+    view = objview.view
+    objview.Destroy()
+    return view
+
+def auth():
+    """
+    Authentication to the server
+    """
+
+    args = cli.get_args()
+
+    try:
+        #service instance
+        si = connect.SmartConnect(host=args.host,
+                                  user=args.user,
+                                  pwd=args.password,
+                                  port=int(args.port))
+
+        if not si:
+            print("Could not connect to the specified host using specified "
+                  "username and password")
+            return None
+
+        atexit.register(connect.Disconnect, si)
+
+    except vmodl.MethodFault, e:
+        print "Caught vmodl fault : " + e.msg
+        return None
+
+    return si.RetrieveContent()
+
+def main():
+    """
+    Contect to the server (vCenter or ESXi host) retrieve the content then
+    drop into a prompt.
+    """
+    global content
+    content = auth()
+    print("== content -> ", content)
+    print("'content = auth()' when re-auth needed")
+
+    code.interact(local=globals())
+    return 0
+
+# Start program
+if __name__ == "__main__":
+    main()
