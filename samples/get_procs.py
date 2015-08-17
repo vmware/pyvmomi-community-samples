586574c983da7405df12235a9184da35a2671291
pathcl <lsanmartinr@gmail.com>
2015-08-17 16:36:27 -0300

    Get vm's proccess through vmware-tools

---
+++ samples/get_procs.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# Luis San Martin - github.com/pathcl
+# Attempts to get current processes on a given VM (uuid)
+# Through pyvmomi and vmware-tools
+# Further reference can be found at
+# https://www.vmware.com/support/developer/converter-sdk/conv50_apireference/vim.vm.guest.ProcessManager.html
+from pyVim import connect
+from pyVmomi import vim
+
+import ssl
+import tools.cli as cli
+import atexit
+
+# Monkey patch SSL
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Setup args function and get arguments
+
+
+def setup_args():
+    parser = cli.build_arg_parser()
+    parser.add_argument('-uuid', '--uuid',
+                        help='file of uuid',
+                        required=True)
+    parser.add_argument('-vmpass', '--vmpass',
+                        help='vm passwd',
+                        required=True)
+    parser.add_argument('-vmuser', '--vmuser',
+                        help='file of uuid',
+                        required=True)
+    my_args = parser.parse_args()
+    return cli.prompt_for_password(my_args)
+
+
+ARGS = setup_args()
+
+
+try:
+    SI = connect.SmartConnect(host=ARGS.host,
+                              user=ARGS.user,
+                              pwd=ARGS.password,
+                              port=ARGS.port)
+
+    content = SI.RetrieveContent()
+    atexit.register(connect.Disconnect, SI)
+    creds = vim.vm.guest.NamePasswordAuthentication(username=ARGS.vmuser,
+                                                    password=ARGS.vmpass)
+
+except IOError:
+    pass
+
+# Main function which tries to find VM through UUID
+
+
+def main(vm):
+    vm = content.searchIndex.FindByUuid(None, ARGS.uuid, True, True)
+    procs = content.guestOperationsManager.processManager.ListProcesses(vm,
+                                                                        creds)
+    vmprocs = [(proc.name, proc.pid, proc.owner) for proc in procs]
+    print("Process for {0}".format(vm.name))
+    print("")
+    print(vmprocs)
+
+if __name__ == '__main__':
+    main(ARGS.uuid)
