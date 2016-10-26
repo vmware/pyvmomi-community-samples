a4be29bb8c93efabf2244cb98d130e8868be690f
nickcooper-zhangtonghao <nickcooper-zhangtonghao@opencloud.tech>
2016-10-26 04:29:54 -0700

    Sample to get vswitches.
    Signed-off-by: nickcooper-zhangtonghao <nickcooper-zhangtonghao@opencloud.tech>

---
+++ samples/get_hosts_vswitch.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+"""
+Written by nickcooper-zhangtonghao
+Github: https://github.com/nickcooper-zhangtonghao
+Email: nickcooper-zhangtonghao@opencloud.tech
+Note: Example code For testing purposes only
+This code has been released under the terms of the Apache-2.0 license
+http://opensource.org/licenses/Apache-2.0
+"""
+
+from __future__ import print_function
+from pyVim.connect import SmartConnect, Disconnect
+from pyVmomi import vim
+import atexit
+import sys
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Arguments for talking to vCenter')
+
+    parser.add_argument('-s', '--host',
+                        required=True,
+                        action='store',
+                        help='vSpehre service to connect to')
+
+    parser.add_argument('-o', '--port',
+                        type=int,
+                        default=443,
+                        action='store',
+                        help='Port to connect on')
+
+    parser.add_argument('-u', '--user',
+                        required=True,
+                        action='store',
+                        help='User name to use')
+
+    parser.add_argument('-p', '--password',
+                        required=True,
+                        action='store',
+                        help='Password to use')
+
+    args = parser.parse_args()
+    return args
+
+
+def GetVMHosts(content):
+    host_view = content.viewManager.CreateContainerView(content.rootFolder,
+                                                        [vim.HostSystem],
+                                                        True)
+    obj = [host for host in host_view.view]
+    host_view.Destroy()
+    return obj
+
+
+def GetHostsSwitches(hosts):
+    hostSwitchesDict = {}
+    for host in hosts:
+        switches = host.config.network.vswitch
+        hostSwitchesDict[host] = switches
+    return hostSwitchesDict
+
+
+def main():
+    args = get_args()
+    serviceInstance = SmartConnect(host=args.host,
+                                   user=args.user,
+                                   pwd=args.password,
+                                   port=443)
+    atexit.register(Disconnect, serviceInstance)
+    content = serviceInstance.RetrieveContent()
+
+    hosts = GetVMHosts(content)
+    hostSwitchesDict = GetHostsSwitches(hosts)
+
+    for host, vswithes in hostSwitchesDict.items():
+        for v in vswithes:
+            print(v.name)
+
+
+# Main section
+if __name__ == "__main__":
+    sys.exit(main())
