c5dc41b809e6beff0f3e505905dbd106dd553b52
Luckylau <laujunbupt0203@163.com>
2016-10-26 20:42:38 +0800

    Sample to  add the virtual ethernet card of VM  to  DistributedVirtualSwitch  PortGroup

--- samples/add_vm_nic_to_dvs.py
+++ samples/add_vm_nic_to_dvs.py
@@ -8,28 +8,28 @@ Email: laujunbupt0913@163.com
 """
 
 
-from pyVim.connect import SmartConnect,Disconnect
+from pyVim.connect import SmartConnect, Disconnect
 import atexit
 from pyVmomi import vim
 import sys
 
 
-def add_nic(vm,mac,port):
-    spec=vim.vm.ConfigSpec()
-    nic_changes=[]
-    nic_spec=vim.vm.device.VirtualDeviceSpec()
-    nic_spec.operation=vim.vm.device.VirtualDeviceSpec.Operation.add
+def add_nic(vm, mac, port):
+    spec = vim.vm.ConfigSpec()
+    nic_changes = []
+    nic_spec = vim.vm.device.VirtualDeviceSpec()
+    nic_spec.operation = vim.vm.device.VirtualDeviceSpec.Operation.add
 
-    nic_spec.device=vim.vm.device.VirtualE1000()
-    nic_spec.device.deviceInfo=vim.Description()
-    nic_spec.device.deviceInfo.summary='vCenter API'
+    nic_spec.device = vim.vm.device.VirtualE1000()
+    nic_spec.device.deviceInfo = vim.Description()
+    nic_spec.device.deviceInfo.summary = 'vCenter API'
 
-    nic_spec.device.backing=vim.vm.device.VirtualEthernetCard.DistributedVirtualPortBackingInfo()
-    nic_spec.device.backing.port=vim.dvs.PortConnection()
-    nic_spec.device.backing.port.portgroupKey=port.portgroupKey
-    nic_spec.device.backing.port.switchUuid=port.dvsUuid
-    nic_spec.device.backing.port.portKey =port.key
-
+    nic_spec.device.backing = \
+        vim.vm.device.VirtualEthernetCard.DistributedVirtualPortBackingInfo()
+    nic_spec.device.backing.port = vim.dvs.PortConnection()
+    nic_spec.device.backing.port.portgroupKey = port.portgroupKey
+    nic_spec.device.backing.port.switchUuid = port.dvsUuid
+    nic_spec.device.backing.port.portKey = port.key
 
     nic_spec.device.connectable = vim.vm.device.VirtualDevice.ConnectInfo()
     nic_spec.device.connectable.startConnected = True
@@ -46,63 +46,66 @@ def add_nic(vm,mac,port):
     e = vm.ReconfigVM_Task(spec=spec)
     print "Nic card added success ..."
 
+
 def get_obj(content, vimtype, name):
     obj = None
     container = content.viewManager.CreateContainerView(
-        content.rootFolder,vimtype,True
+        content.rootFolder, vimtype, True
     )
     for c in container.view:
-        if c.name==name:
+        if c.name == name:
             obj=c
             break
     return obj
 
-def search_port(dvs,portgroupkey):
-    search_portkey=[]
-    criteria= vim.dvs.PortCriteria()
-    criteria.connected=False
+
+def search_port(dvs, portgroupkey):
+    search_portkey = []
+    criteria = vim.dvs.PortCriteria()
+    criteria.connected = False
     criteria.inside = True
     criteria.portgroupKey = portgroupkey
-    ports=dvs.FetchDVPorts(criteria)
+    ports = dvs.FetchDVPorts(criteria)
     for port in ports:
         search_portkey.append(port.key)
     print search_portkey
     return search_portkey[0]
 
+
 def get_args():
     if len(sys.argv)>1:
-        host,user,password,vm_name,port_group,macAddress=sys.argv[1:]
+        host,user,password,vm_name,port_group,macAddress = sys.argv[1:]
     else:
-        host=raw_input("Vcenter IP : ")
-        user=raw_input("User: ")
-        password=raw_input("Password: ")
-        vm_name=raw_input("VM_name: ")
-        port_group=raw_input("Port_Group: ")
-        macAddress=raw_input("Input MacAddress :")
-    return host,user,password,vm_name,port_group,macAddress
+        host = raw_input("Vcenter IP : ")
+        user = raw_input("User: ")
+        password = raw_input("Password: ")
+        vm_name = raw_input("VM_name: ")
+        port_group = raw_input("Port_Group: ")
+        macAddress = raw_input("Input MacAddress :")
+    return host, user, password, vm_name, port_group, macAddress
 
 def port_find(dvs, key):
     obj = None
-    ports=dvs.FetchDVPorts()
+    ports = dvs.FetchDVPorts()
     for c in ports:
-        if c.key==key:
-            obj=c
+        if c.key == key:
+            obj = c
     return obj
 
 def main():
-    host,user,password,vm_name,port_group,macAddress=get_args()
-    default_port="443"
+    host, user, password, vm_name, port_group, macAddress = get_args()
+    default_port = "443"
 
-    serviceInstance=SmartConnect(host=host,
-                                 user=user,
-                                 pwd=password,
-                                 port=default_port)
+    serviceInstance = SmartConnect(host = host,
+                                 user = user,
+                                 pwd = password,
+                                 port = default_port)
 
-    atexit.register(Disconnect,serviceInstance)
+    atexit.register(Disconnect, serviceInstance)
     content = serviceInstance.RetrieveContent()
 
     print "Search VDS PortGroup by Name ..."
-    portgroup=None
+    portgroup = None
     portgroup = get_obj(content, [vim.dvs.DistributedVirtualPortgroup], port_group)
     if portgroup is None:
         print "Portgroup not Found in DVS ..."
@@ -110,15 +113,15 @@ def main():
 
     print "Search Available(Unused) port for VM..."
     dvs = portgroup.config.distributedVirtualSwitch
-    portKey=search_port(dvs,portgroup.key)
+    portKey = search_port(dvs,portgroup.key)
     port = port_find(dvs, portKey)
 
     print "Search VM by Name ..."
-    vm=None
-    vm=get_obj(content,[vim.VirtualMachine],vm_name)
+    vm = None
+    vm = get_obj(content, [vim.VirtualMachine], vm_name)
     if vm:
         print "Find Vm , Add Nic Card ..."
-        add_nic(vm,macAddress,port)
+        add_nic(vm, macAddress, port)
     else:
         print "Vm not Found ..."
 
