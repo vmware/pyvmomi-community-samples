ad2b6983cc0513f9fe3d9cec6901d7fd1f6eaa05
Nathan Prziborowski <prziborowski@gmail.com>
2017-02-18 08:11:48 +0900

    Update to use SmartConnectNoSSL and update description

--- samples/filter_vms.py
+++ samples/filter_vms.py
@@ -6,13 +6,24 @@ Github: https://github.com/prziborowski
 This code is released under the terms of the Apache 2
 http://www.apache.org/licenses/LICENSE-2.0.html
 
-Example script to filter the list of VMs by a property's value.
-Defaults for powered on VMs.
+The property collector can be used to fetch a subset of properties
+for a large amount of objects with fewer round trips that iterating.
+
+This sample shows how it could be used to fetch the power state of
+all the VMs and post-process filter on them.
+
+Note the printing vm.name does cause round-trip per-VM, so it could
+be extended in a real script/product to fetch the name property as
+well.
+
+I used the ViewManager to gather VMs as it seems easier than making
+a traverse spec go through all the datacenters to gather VMs that
+may also be in sub-folders.
 
 """
 import sys
 from pyVmomi import vim, vmodl
-from pyVim.connect import SmartConnect, Disconnect
+from pyVim.connect import SmartConnectNoSSL, Disconnect
 from pyVim.task import WaitForTask
 from tools import cli
 
@@ -60,7 +71,10 @@ def filter_results(result, value):
 
 def main():
     args = setup_args()
-    si = SmartConnect(host=args.host, user=args.user, pwd=args.password)
+    si = SmartConnectNoSSL(host=args.host,
+                           user=args.user,
+                           pwd=args.password,
+                           port=args.port)
     # Start with all the VMs from container, which is easier to write than
     # PropertyCollector to retrieve them.
     vms = get_obj(si, si.content.rootFolder, [vim.VirtualMachine])
