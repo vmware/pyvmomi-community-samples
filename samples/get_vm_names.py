e80a1ac6c06512c75b69d60f28597033a3414ce3
RJ Lalumiere <rj.lalumiere@dealer.com>
2017-03-03 15:07:04 -0500

    Address some comments from PR#340

--- samples/get_vm_names.py
+++ samples/get_vm_names.py
@@ -1,30 +1,27 @@
-#!/usr/bin/python
-# VMware vSphere Python SDK
-# Copyright (c) 2008-2015 VMware, Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+#!/usr/bin/env python
 """
 Python program for flat text listing the VMs on an
-ESX / vCenter, host one per line
-"""
+ESX / vCenter, host one per line.
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 from __future__ import print_function
 import atexit
 from pyVim.connect import SmartConnectNoSSL, Disconnect
 from pyVmomi import vim
 from tools import cli
 
+MAX_DEPTH = 10
 
 def setup_args():
 
@@ -42,12 +39,11 @@ def printvminfo(vm, depth=1):
     Print information for a particular virtual machine or recurse into a folder
     with depth protection
     """
-    maxdepth = 10
 
     # if this is a group it will have children. if it does, recurse into them
     # and then return
     if hasattr(vm, 'childEntity'):
-        if depth > maxdepth:
+        if depth > MAX_DEPTH:
             return
         vmlist = vm.childEntity
         for child in vmlist:
@@ -72,9 +68,6 @@ def main():
                                port=int(args.port))
         atexit.register(Disconnect, si)
     except vim.fault.InvalidLogin:
-        pass
-
-    if not si:
         raise SystemExit("Unable to connect to host "
                          "with supplied credentials.")
 
@@ -86,7 +79,6 @@ def main():
             vmlist = vmfolder.childEntity
             for vm in vmlist:
                 printvminfo(vm)
-    return 0
 
 # Start program
 if __name__ == "__main__":
