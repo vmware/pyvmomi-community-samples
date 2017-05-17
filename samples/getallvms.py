36e5579883cce237e90c7ff70154b6d8b73cb2c7
Leandro Ferreira da Silva <ldfsilva@gmail.com>
2017-05-17 16:10:50 -0500

    Fix ssl certificate verification error issue#359 (#367)
    * Fix ssl certificate verification error issue#359
    
    * Make usage of SmartConnectNoSSL instead of sslContext to disable SSL verification
    
    * Fix code to be pep8 compliant

--- samples/getallvms.py
+++ samples/getallvms.py
@@ -67,10 +67,16 @@ def main():
     args = cli.get_args()
 
     try:
-        service_instance = connect.SmartConnect(host=args.host,
-                                                user=args.user,
-                                                pwd=args.password,
-                                                port=int(args.port))
+        if args.disable_ssl_verification:
+            service_instance = connect.SmartConnectNoSSL(host=args.host,
+                                                         user=args.user,
+                                                         pwd=args.password,
+                                                         port=int(args.port))
+        else:
+            service_instance = connect.SmartConnect(host=args.host,
+                                                    user=args.user,
+                                                    pwd=args.password,
+                                                    port=int(args.port))
 
         atexit.register(connect.Disconnect, service_instance)
 
