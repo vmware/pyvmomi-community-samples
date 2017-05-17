d377e8b875a8f6a7f264fdbd459dbc956ce44d52
William Lam <william2003@gmail.com>
2017-05-16 18:12:20 -0700

    Fixed indentation

--- samples/update_esxi_advanced_settings.py
+++ samples/update_esxi_advanced_settings.py
@@ -98,9 +98,9 @@ def main():
     args = get_args()
     try:
         service_instance = connect.SmartConnectNoSSL(host=args.host,
-                                                user=args.user,
-                                                pwd=args.password,
-                                                port=int(args.port))
+                                                     user=args.user,
+                                                     pwd=args.password,
+                                                     port=int(args.port))
         if not service_instance:
             print("Could not connect to the specified host using specified "
                   "username and password")
