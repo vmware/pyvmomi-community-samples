c0562cd3cfbe7cee66e750424192f9431862b2f0
mindheist <dragonzsnake@gmail.com>
2017-11-16 18:42:40 -0800

    removed lines that accidentally got added during a merge

--- samples/upload_file_to_vm.py
+++ samples/upload_file_to_vm.py
@@ -74,8 +74,7 @@ def get_obj(content, vimtype, name):
             obj = c
             break
     return obj
-<<<<<<< Updated upstream
-=======
+
 
 def get_obj(content, vimtype, name):
     """
@@ -92,7 +91,6 @@ def get_obj(content, vimtype, name):
             break
     return obj
 
->>>>>>> Stashed changes
 
 def main():
     """
@@ -116,16 +114,8 @@ def main():
             vm = get_obj(content, [vim.VirtualMachine], args.vm_name)
 
         if vm is None:
-<<<<<<< Updated upstream
-            raise SystemExit("VM not found,verify the UUID or the name of the VM provided")
-=======
-<<<<<<< HEAD
             raise SystemExit("VM not found,verify the UUID or the \
                 name of the VM provided")
-=======
-            raise SystemExit("VM not found,verify the UUID or the name of the VM provided")
->>>>>>> origin/upload_file_to_vm_fix
->>>>>>> Stashed changes
         tools_status = vm.guest.toolsStatus
         if (tools_status == 'toolsNotInstalled' or
                 tools_status == 'toolsNotRunning'):
