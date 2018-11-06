4180371d61e0af15adb172b1236096f0c85407e6
Alexander Todorov <atodorov@redhat.com>
2018-11-06 07:20:52 -0500

    Refactor scheduled_poweroff
    - allow to disable SSL verification
    - re-use cli.build_arg_parser()
    - make --date not mandatory and execute power off immediately
      if value is not set

--- samples/scheduled_poweroff.py
+++ samples/scheduled_poweroff.py
@@ -9,54 +9,52 @@ http://www.apache.org/licenses/LICENSE-2.0.html
 Example code for using the task scheduler.
 """
 
+import ssl
 import atexit
-import argparse
-import getpass
 from datetime import datetime
+
 from pyVmomi import vim
 from pyVim import connect
 
+from tools import cli
+
 
 def get_args():
-    parser = argparse.ArgumentParser(
-        description='Arguments for scheduling a poweroff of a virtual machine')
-    parser.add_argument('-s', '--host', required=True, action='store',
-                        help='Remote host to connect to')
-    parser.add_argument('-o', '--port', type=int, default=443, action='store',
-                        help='Port to connect on')
-    parser.add_argument('-u', '--user', required=True, action='store',
-                        help='User name to use when connecting to host')
-    parser.add_argument('-p', '--password', required=False, action='store',
-                        help='Password to use when connecting to host')
-    parser.add_argument('-d', '--date', required=True, action='store',
+    parser = cli.build_arg_parser()
+
+    parser.add_argument('-d', '--date', required=False, action='store',
                         help='Date and time used to create the scheduled task '
-                        'with the format d/m/Y H:M')
+                        'with the format d/m/Y H:M. If not specified execute '
+                        'task immediately')
     parser.add_argument('-n', '--vmname', required=True, action='store',
                         help='VM name on which the action will be performed')
     args = parser.parse_args()
-    return args
+    return cli.prompt_for_password(args)
 
 
 def main():
     args = get_args()
-    try:
-        dt = datetime.strptime(args.date, '%d/%m/%Y %H:%M')
-    except ValueError:
-        print('Unrecognized date format')
-        raise
-        return -1
 
-    if args.password:
-        password = args.password
-    else:
-        password = getpass.getpass(prompt='Enter password for host %s and '
-                                   'user %s: ' % (args.host, args.user))
+    dt = None
+    if args.date:
+        try:
+            dt = datetime.strptime(args.date, '%d/%m/%Y %H:%M')
+        except ValueError:
+            print('Unrecognized date format')
+            return -1
+
+    sslContext = None
+
+    if args.disable_ssl_verification:
+        sslContext = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        sslContext.verify_mode = ssl.CERT_NONE
 
     try:
-        si = connect.SmartConnectNoSSL(host=args.host,
-                                       user=args.user,
-                                       pwd=password,
-                                       port=int(args.port))
+        si = connect.SmartConnect(host=args.host,
+                                  user=args.user,
+                                  pwd=args.password,
+                                  port=int(args.port),
+                                  sslContext=sslContext)
     except vim.fault.InvalidLogin:
         print("Could not connect to the specified host using specified "
               "username and password")
@@ -79,7 +77,8 @@ def main():
     spec.name = 'PowerOff vm %s' % args.vmname
     spec.description = ''
     spec.scheduler = vim.scheduler.OnceTaskScheduler()
-    spec.scheduler.runAt = dt
+    if dt:
+        spec.scheduler.runAt = dt
     spec.action = vim.action.MethodAction()
     spec.action.name = vim.VirtualMachine.PowerOff
     spec.enabled = True
