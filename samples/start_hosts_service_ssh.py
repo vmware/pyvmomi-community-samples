095792badc01bef42975b3e81a73677d32899e14
Maros Kukan <maros.kukan@me.com>
2023-12-14 15:34:58 +0100

    Add hosts services and sample ssh start and stop

---
+++ samples/start_hosts_service_ssh.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from pyVmomi import vim
+from tools import cli, service_instance
+import sys
+
+def get_vm_hosts(content):
+    host_view = content.viewManager.CreateContainerView(content.rootFolder,
+                                                        [vim.HostSystem],
+                                                        True)
+    hosts = list(host_view.view)
+    host_view.Destroy()
+    return hosts
+
+def start_ssh_service(host):
+    service_system = host.configManager.serviceSystem
+    services = service_system.serviceInfo.service
+
+    for service in services:
+        if service.key == "TSM-SSH":
+            if not service.running:
+                print(f"Starting SSH service on {host.name}")
+                service_system.StartService(service.key)
+                print("SSH service started successfully.")
+            else:
+                print(f"SSH service is already running on {host.name}")
+            return
+
+    print("SSH service not found on the host.")
+
+def main():
+    parser = cli.Parser()
+    args = parser.get_args()
+    si = service_instance.connect(args)
+    content = si.RetrieveContent()
+
+    hosts = get_vm_hosts(content)
+
+    if hosts:
+        for host in hosts:
+            start_ssh_service(host)
+
+if __name__ == "__main__":
+    sys.exit(main())
+
---
+++ samples/stop_hosts_service_ssh.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from pyVmomi import vim
+from tools import cli, service_instance
+import sys
+
+def get_vm_hosts(content):
+    host_view = content.viewManager.CreateContainerView(content.rootFolder,
+                                                        [vim.HostSystem],
+                                                        True)
+    hosts = list(host_view.view)
+    host_view.Destroy()
+    return hosts
+
+def stop_ssh_service(host):
+    service_system = host.configManager.serviceSystem
+    services = service_system.serviceInfo.service
+
+    for service in services:
+        if service.key == "TSM-SSH":
+            if service.running:
+                print(f"Stopping SSH service on {host.name}")
+                service_system.StopService(service.key)
+                print("SSH service stopped successfully.")
+            else:
+                print(f"SSH service is already stopped on {host.name}")
+            return
+
+    print("SSH service not found on the host.")
+
+def main():
+    parser = cli.Parser()
+    args = parser.get_args()
+    si = service_instance.connect(args)
+    content = si.RetrieveContent()
+
+    hosts = get_vm_hosts(content)
+
+    if hosts:
+        for host in hosts:
+            stop_ssh_service(host)
+
+if __name__ == "__main__":
+    sys.exit(main())
+
